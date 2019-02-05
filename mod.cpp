d3c3a9876507d087039aa0fb0d208aae34d6f2a8
Adam <32800613+Adde2000@users.noreply.github.com>
2019-02-05 23:50:01 +0100

    Added files

---
+++ mod.cpp
@@ -0,0 +1,20 @@
+/// Name of your mod
+name = "UNS Pack";
+/// Picture displayed from the expansions menu/ Optimal size is 2048x1024, other sizes work too
+picture = "ws_rhs.paa"; 
+/// Display next to the item added by the mod
+logoSmall = "ws_rhs.paa";
+/// Logo displayed in the main menu
+logo = "ws_rhs.paa";
+/// When the mouse is over, in the main menu
+logoOver = "ws_rhs.paa";
+/// Website URL, that can accessed from the expansions menu
+action = "https://www.unitedsurvivors.org/";
+/// Tool tip displayed when the mouse is left over, in the main menu
+tooltipOwned = "Hello!";
+/// Overview text, displayed from the extension menu
+overview = "Made by Adde";
+/// Hide the extension name 
+hideName = 0;
+/// Hide the extension menu
+hidePicture = 0;
