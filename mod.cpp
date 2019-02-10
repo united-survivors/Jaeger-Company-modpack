a9877b3e9ff216bd8683e58b7f976805c2a97ecb
Adam <32800613+Adde2000@users.noreply.github.com>
2019-02-10 14:58:13 +0100

    Update mod.cpp

--- mod.cpp
+++ mod.cpp
@@ -1,20 +1,11 @@
-/// Name of your mod
 name = "Jaeger Company modpack";
-/// Picture displayed from the expansions menu/ Optimal size is 2048x1024, other sizes work too
 picture = "jaeger_logo.paa"; 
-/// Display next to the item added by the mod
 logoSmall = "jaeger_logo.paa";
-/// Logo displayed in the main menu
 logo = "jaeger_logo.paa";
-/// When the mouse is over, in the main menu
 logoOver = "jaeger_logo.paa";
-/// Website URL, that can accessed from the expansions menu
-action = "https://www.unitedsurvivors.org/arma3";
-/// Tool tip displayed when the mouse is left over, in the main menu
-tooltipOwned = "Hello!";
-/// Overview text, displayed from the extension menu
-overview = "Made by Adde";
-/// Hide the extension name 
+actionName = "GitHub";
+action = "https://github.com/united-survivors/Jaeger-Company-modpack";
+tooltipOwned = "Jaeger Pack Owned";
+overview = "A modpack made for Jaeger Company, a unit within United Survivors";
 hideName = 0;
-/// Hide the extension menu
 hidePicture = 0;
